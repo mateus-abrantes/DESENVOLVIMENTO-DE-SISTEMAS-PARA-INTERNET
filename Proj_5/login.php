25bdfe6486af9e3f33e8f27fa451760d4942e955
mateus-abrantes <mateus.abrantes7@gmail.com>
2019-05-02 10:19:38 -0300

    Add files

---
+++ Proj_5/login.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-BR" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>Home</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="canonical" href="https://getbootstrap.com/docs/4.3/examples/sign-in/">
+</head>
+<body class="text-center">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm mt-5">
+      </div>
+      <div class="col-sm mt-4 jumbotron">
+        <form class="form">
+          <h1 class="h3 mb-3 font-weight-normal">Login</h1>
+          <label for="usuario" class="sr-only">Usuario</label>
+          <input type="text" id="usuario" class="form-control" placeholder="Usuario" required="" autofocus="">
+          <label for="senha" class="sr-only">Senha</label>
+          <input type="password" id="senha" class="form-control" placeholder="Senha" required="">
+          <button class="btn btn-lg btn-primary btn-block mt-3" type="submit">Logar</button>
+          <p class="mt-5 mb-3 text-muted">© 2019</p>
+        </form>
+      </div>
+      <div class="col-sm mt-5">
+      </div>
+    </div>
+  </div>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
